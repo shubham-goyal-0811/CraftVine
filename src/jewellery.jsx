bc0288966cdd43f272a9b6b99f550e817132407b
sidharth sharma <sidharthsharma17240@gmail.com>
2024-04-27 16:21:22 +0530

    different pages setup

---
+++ src/jewellery.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const jewellery= ()=>{
+   return (
+      <>
+        <h1>hello jwellery</h1>
+      </>
+   )
+} 
+export default jewellery;
---
+++ src/kitchen_and_dinning.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const kitchen_and_dinning= ()=>{
+   return (
+      <>
+        <h1>hello kitchen_and_dinning</h1>
+      </>
+   )
+} 
+export default kitchen_and_dinning;
