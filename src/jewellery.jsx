b82a0827b12c799dcdc49ec35b05809b73574a57
shubham-goyal-0811 <shubhamsubbi1@gmail.com>
2024-04-28 23:08:05 +0530

    conflict resolved

--- src/jewellery.jsx
+++ src/jewellery.jsx
@@ -2,19 +2,7 @@ import './home';
 const jewellery= ()=>{
    return (
       <>
-     <section id="header">
-        <a href="#"><img src={'src/images/craftVine-logo-removebg-preview (1).png'} /></a>
-        <div>
-          <ul id="navbar">
-            <li><a href="/" className='active'>Home</a></li>
-            <li><a href="#catogoriesitems">Shop By Catogory</a></li>
-            <li><a href="#editorpicks">Editor's choice</a></li>
-            <li><a href="#">Contact</a></li>
-            <li><a href="">Login & Sign-up</a></li>
-            <li><a href="#"><FontAwesomeIcon icon={faShoppingCart} /></a></li>
-          </ul>
-        </div>
-      </section>
+        <h1 className="bg">hello jwellery</h1>
       </>
    )
 } 
